506647f1871e67daff44bd3d82a3549cca3cca1e
Rogith-Ranger <rogithy@gmail.com>
2020-02-11 23:04:14 +0530

    first commit

---
+++ precache-manifest.771b2dacf2b9f7595a99cdfc42af4f25.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "48138bceaa4ac479aec01b08add24be5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6ff425312e0c1ba6f3a9",
+    "url": "/static/css/main.895c4aba.chunk.css"
+  },
+  {
+    "revision": "bde1b4e6a0be72b091c6",
+    "url": "/static/js/2.6bbf566e.chunk.js"
+  },
+  {
+    "revision": "92def7aeebc94040ee9ce15d746dabef",
+    "url": "/static/js/2.6bbf566e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6ff425312e0c1ba6f3a9",
+    "url": "/static/js/main.e57d6e94.chunk.js"
+  },
+  {
+    "revision": "9069b9763e4e9c5aec0e",
+    "url": "/static/js/runtime-main.09de0777.js"
+  }
+]);
